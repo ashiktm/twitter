19c0f2fde0bd7c696fd7cdf577dcbdced72b85b0
ashik <ashiktm.ind@gmail.com>
2023-07-23 23:06:10 +0530

    added home page header, main section

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { LandingComponent } from './landing/landing.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
-    LandingComponent
+    LandingComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     CommonModule,
